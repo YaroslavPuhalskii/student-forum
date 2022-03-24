6963c3a7bae8794f044bc0ccd148f468bcf52095
yaroslav <netpywka14@gmail.com>
2022-03-24 16:13:02 +0300

    Added controllers and models for auth.

---
+++ api/StudentForum.Api/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentForum.Api.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        protected BaseController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        protected IMapper Mapper => _mapper;
+    }
+}
