7c6993e0d21ea5a156322e7c19a12e5c50e1c92a
yaroslav <netpywka14@gmail.com>
2022-03-24 14:28:41 +0300

    Added auth and register models.

---
+++ api/StudentForum.BL/Models/Authorization/AuthInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentForum.BL.Models.Authorization
+{
+    public class AuthInfo
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
